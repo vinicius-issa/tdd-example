469e864571f080668eb7eead512e3d0d7d3974c3
Vinicius de Almeida Issa <vinicius.almeida@rockcontent.com>
2021-07-22 18:02:58 -0300

    test: ensure GitRock calls Cache.get with correct values

--- src/git-rock.spec.ts
+++ src/git-rock.spec.ts
@@ -1,8 +1,33 @@
 import { GitRock } from './git-rock'
+import { RockCache } from './protocols/rock-cache'
 
 describe('GitRock', () => {
-  test('', async () => {
-    const sut = new GitRock()
+  const makeRockCacheMock = () => {
+    class RockCacheMock implements RockCache {
+      async get (key: string): Promise<string|undefined> {
+        return
+      }
+
+      async set (key: string, value: string): Promise<void> {
+        return
+      }
+    }
+    return new RockCacheMock()
+  }
+
+  const makeSut = () => {
+    const cacheMock = makeRockCacheMock()
+    const sut = new GitRock(cacheMock)
+    return {
+      cacheMock,
+      sut
+    }
+  }
+
+  test('Should getStars return the correct values', async () => {
+    const {
+      sut
+    } = makeSut()
     const repositories = await sut.getStars('any_company')
     expect(repositories).toEqual([
       {
@@ -14,5 +39,16 @@ describe('GitRock', () => {
         stars: 15
       }
     ])
+  })
+
+  test('Should call get in CacheMock with correct value', async () => {
+    const {
+      sut,
+      cacheMock
+    } = makeSut()
+
+    const cacheSpy = jest.spyOn(cacheMock, 'get')
+    await sut.getStars('any_company')
+    expect(cacheSpy).toBeCalledWith('any_company')
   })
 })
